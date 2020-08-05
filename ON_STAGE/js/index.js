ce140dda55e2ab05b839676ac904520130ebbb15
yuppe <60644967+yukikosuge@users.noreply.github.com>
2020-08-05 19:41:08 +0900

    Add files via upload

---
+++ ON_STAGE/js/index.js
@@ -0,0 +1,15 @@
+
+
+//  <script>
+// $(document).ready(function() {
+// $('#fullpage').fullpage({
+// scrollingSpeed: 8000, //スクロールするスピードをミリ秒で設定・デフォルトは700
+// easing: easeInSine, //垂直方向と水平方向のスクロールに使用するトランジションエフェクトを定義・デフォルトはeaseInOutCubic
+// loopBottom: true, //ラストページまでスクロールダウンした時に、トップページに戻るループするかどうかを設定・デフォルトはfalse
+// autoScrolling: true, //少しスクロールした時に自動で次のページまでスクロースするかどうかを設定・デフォルトはtrue
+// scrollBar: false, //スクロールバーの表示/非表示の設定・デフォルトはfalse
+// navigation: false, //右側に黒丸のナビゲーションの表示/非表示の設定・デフォルトはfalse
+// scrollOverflow: true //内容がページの高さより大きい場合に、スクロールを作成するべきかどうかを設定・デフォルトはfalse
+// });
+// });
+// </script> 
